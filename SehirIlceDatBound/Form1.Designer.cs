77dd297ec8b38cc63c8946790cb4c75aec9dcd7c
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2021-12-18 10:40:05 +0300

    First Commit

---
+++ SehirIlceDatBound/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SehirIlceDatBound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_sehir = new System.Windows.Forms.ComboBox();
+            this.cb_ilce = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tb_adres = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_gonder = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehir :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İlçe :";
+            // 
+            // cb_sehir
+            // 
+            this.cb_sehir.FormattingEnabled = true;
+            this.cb_sehir.Location = new System.Drawing.Point(67, 30);
+            this.cb_sehir.Name = "cb_sehir";
+            this.cb_sehir.Size = new System.Drawing.Size(190, 21);
+            this.cb_sehir.TabIndex = 2;
+            // 
+            // cb_ilce
+            // 
+            this.cb_ilce.FormattingEnabled = true;
+            this.cb_ilce.Location = new System.Drawing.Point(67, 57);
+            this.cb_ilce.Name = "cb_ilce";
+            this.cb_ilce.Size = new System.Drawing.Size(190, 21);
+            this.cb_ilce.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_gonder);
+            this.groupBox1.Controls.Add(this.tb_adres);
+            this.groupBox1.Controls.Add(this.cb_sehir);
+            this.groupBox1.Controls.Add(this.cb_ilce);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 245);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adres";
+            // 
+            // tb_adres
+            // 
+            this.tb_adres.Location = new System.Drawing.Point(67, 84);
+            this.tb_adres.Multiline = true;
+            this.tb_adres.Name = "tb_adres";
+            this.tb_adres.Size = new System.Drawing.Size(190, 66);
+            this.tb_adres.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Adres :";
+            // 
+            // btn_gonder
+            // 
+            this.btn_gonder.Location = new System.Drawing.Point(67, 156);
+            this.btn_gonder.Name = "btn_gonder";
+            this.btn_gonder.Size = new System.Drawing.Size(190, 23);
+            this.btn_gonder.TabIndex = 4;
+            this.btn_gonder.Text = "Gönder";
+            this.btn_gonder.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_sehir;
+        private System.Windows.Forms.ComboBox cb_ilce;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_gonder;
+        private System.Windows.Forms.TextBox tb_adres;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
